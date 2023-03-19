90f22554bbc08108a8d8dd39161a80940618947c
Alex de la Iglesia <alexander.delaiglesia@gmail.com>
2023-03-19 18:39:48 -0400

    working on bert

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import tensorflow_hub as hub
+import tensorflow as tf
+# import tfa.text as text
+import tensorflow_text as text
 
- 
+BERT_URL = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4"
+PREPROCESS_URL = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
+text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
+preprocessor = hub.KerasLayer(PREPROCESS_URL)
+text_test = ['this is such an amazing movie!']
+text_preprocessed = preprocessor(text_test)
+
+print(f'Keys       : {list(text_preprocessed.keys())}')
+print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
+print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
+print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
+print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
